2b9563c509cf2714ac0ec5b2a65267406492dba4
Sujal Malhotra <msujal29012004@gmail.com>
2024-09-17 02:02:41 +0530

    Added HTML code and did some planning of the features

---
+++ index.js
@@ -0,0 +1,14 @@
+// Have random colors
+// Show complete note on hover else show fixed length of the note
+// Make it look like actual notes by adding a note at the center of it
+// Use font as a handwriting like notes
+// Priority based coloring\
+// Mark them as Todo, Doing or Done (Make particular styling for the same)
+// Create a note
+// Update a note
+// Delete a note
+// Drag and drop a note
+// Add light and Dark theme
+// Add short cuts a edit a note
+// Ctrl + N to create a new Note
+// We can create collection of notes also
