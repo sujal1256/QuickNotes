f5f2f3d4c2e6fd8e3e4654e4ad4b47937339a04d
Sujal Malhotra <msujal29012004@gmail.com>
2024-09-17 17:36:15 +0530

    Priority based color and timestamps

---
+++ constants.js
@@ -0,0 +1,18 @@
+export const notes = [];
+
+export const months = [
+  "Jan",
+  "Feb",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "Aug",
+  "Sept",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+
+export const priorityBasedColouring = ["#86D293", "#7695FF", "#C96868"];
--- index.js
+++ index.js
@@ -1,11 +1,16 @@
-"use strict";
-// Have random colors
+import { months, notes, priorityBasedColouring } from "./constants.js";
+("use strict");
+// Have random colors.....
+// Use font as a handwriting like notes.......
+// Priority based coloring......
+// Create a note.....
+
+
+
+
 // Show complete note on hover else show fixed length of the note
 // Make it look like actual notes by adding a note at the center of it
-// Use font as a handwriting like notes
-// Priority based coloring\
 // Mark them as Todo, Doing or Done (Make particular styling for the same)
-// Create a note
 // Update a note
 // Delete a note
 // Drag and drop a note
@@ -17,44 +22,96 @@
 
 // Sort by time created / priority basis
 
-function closeCreateForm(){
-    opaqueScreen.classList.add('hidden');
+function closeCreateForm() {
+  opaqueScreen.classList.add("hidden");
 }
+
+function giveColorBasedOnPriority(e) {
+  const selectedPriority = e.target.value;
+  
+  // Get the closest note div
+  const closestNote = e.target.closest(".note");
+
+  // Reset note background color
+  closestNote.style.backgroundColor = priorityBasedColouring[Number(selectedPriority)];
+
+
+}
+
+
+
 
 
 const createNoteBtn = document.querySelector(".create-btn");
 const notesSection = document.querySelector(".notes-section");
-const openCreateSection = document.querySelector('.open-create-form');
-const createNoteText = document.querySelector(".create-note-textarea");
+const openCreateSection = document.querySelector(".open-create-form");
+const createNoteTextarea = document.querySelector(".create-note-textarea");
 const createSectionCloseBtn = document.querySelector(
   ".create-section-close-btn"
 );
 const opaqueScreen = document.querySelector(".opaque-screen");
+const createSectionPriotity = document.querySelector(
+  ".create-priority-btn .create-note-priority"
+);
+const priorityDropdowns = document.querySelectorAll(".create-note-priority");
 
 
-console.log(createSectionCloseBtn);
-console.log(opaqueScreen);
 
-openCreateSection.addEventListener('click', (e)=>{
-    e.preventDefault();
 
-    opaqueScreen.classList.remove('hidden');
-})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Loop through all priority dropdowns and add event listeners
+// priorityDropdowns.forEach((dropdown) => {
+//   dropdown.addEventListener("change", ();
+// });
+
+
+openCreateSection.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  opaqueScreen.classList.remove("hidden");
+});
 
 createSectionCloseBtn.addEventListener("click", () => {
-    closeCreateForm();
+  closeCreateForm();
 });
 
 createNoteBtn.addEventListener("click", (e) => {
   e.preventDefault();
-  
+  const date = new Date().getDate();
+  const month = new Date().getMonth();
+  const year = new Date().getFullYear();
+  const time = new Date().toLocaleTimeString();
+
+  const timestamp = `${date} ${months[month]}, ${year} ${time}`;
+
+  console.log(month);
+
+  if (createNoteTextarea?.value?.trim() === "") {
+    // FIXME:
+    alert("Textarea can not be empty");
+
+    return;
+  }
+
   const html = `
           <div class="note">
         <h3 class="note-title">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde magni
-          omnis odio ducimus dolorum officiis voluptatibus voluptatem eaque
-          asperiores corporis repellat dignissimos, velit cumque sunt neque
-          harum consequatur et esse autem illo!
+          ${createNoteTextarea.value}
         </h3>
 
         <!-- delete and edit buttons -->
@@ -68,22 +125,35 @@ createNoteBtn.addEventListener("click", (e) => {
         </div>
 
         <div class="priority-and-status-section">
-          <select class="priority" name="priority">
-            <option value="" disabled selected>Select Priority</option>
-            <option value="high" priority_value="3">High</option>
-            <option value="medium" priority_value="2">Medium</option>
-            <option value="low" priority_value="1">Low</option>
+          <select class="priority create-note-priority">
+            <option value="-1" disabled selected>Select Priority</option>
+            <option value="2">High</option>
+            <option value="1">Medium</option>
+            <option value="0">Low</option>
           </select>
           <p>Status</p>
         </div>
 
         <div class="timestamp-div">
           <p class="timestamp-status">Created on:</p>
-          <p class="timestamp">19th Sept, 2024 20:09:26</p>
+        <p class="timestamp">${date} ${months[month]}, ${year} ${time}</p>
         </div>
       </div>
     `;
 
   notesSection.insertAdjacentHTML("beforeend", html);
+
+  // Add changing colors based on priority
+  const newPriorityDropdown = notesSection.lastElementChild.querySelector(".create-note-priority");
+  newPriorityDropdown.addEventListener("change", giveColorBasedOnPriority);
+
+  notes.push({
+    timestamp,
+    content: createNoteTextarea.value,
+    priority: "-1",
+  });
+
+
+  console.log(notes);
   closeCreateForm();
 });
